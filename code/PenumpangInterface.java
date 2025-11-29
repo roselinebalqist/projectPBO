974d505c84e5cefce7419916b5aa5882b8046f51
roselinebalqist <alinbalqist@gmail.com>
2025-11-29 11:38:32 +0700

    Create PenumpangInterface.java

---
+++ code/PenumpangInterface.java
@@ -0,0 +1,8 @@
+public interface PenumpangInterface {
+  boolean bisaDudukPrioritas();
+  boolean bayarOngkos(int ongkos);
+  int getUmur();
+  boolean isHamil();
+  TipePenumpang getTipe();
+  String getId();
+}
