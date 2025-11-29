14f5715cf79d4367cb25892bc162d13cc1d833c4
roselinebalqist <alinbalqist@gmail.com>
2025-11-29 12:35:58 +0700

    Update PenumpangBiasa.java

--- code/PenumpangBiasa.java
+++ code/PenumpangBiasa.java
@@ -1,1 +1,13 @@
+public class PenumpangBiasa extends Penumpang {
+    public PenumpangBiasa(String id, int umur, boolean hamil, int saldo) {
+        super(id, umur, hamil, saldo);
+    }
 
+    public boolean bisaDudukPrioritas() {
+        return false; 
+    }
+
+    public TipePenumpang getTipe() {
+        return TipePenumpang.BIASA;
+    }
+}
