3c9f00c66648f1bb25579c19241becdfb6350929
roselinebalqist <alinbalqist@gmail.com>
2025-11-29 12:35:10 +0700

    Update Penumpang.java

--- code/Penumpang.java
+++ code/Penumpang.java
@@ -1,1 +1,12 @@
+public abstract class Penumpang implements PenumpangInterface {
+    protected String id;
+    protected int umur;
+    protected boolean hamil;
+    protected int saldo;
 
+    public Penumpang(String id, int umur, boolean hamil, int saldo) {
+        this.id = id;
+        this.umur = umur;
+        this.hamil = hamil;
+        this.saldo = saldo;
+    }
