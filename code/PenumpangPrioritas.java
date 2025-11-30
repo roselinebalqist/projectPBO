85434c2fea1a947c0389390e3b6460c76a74cb67
roselinebalqist <alinbalqist@gmail.com>
2025-11-30 15:53:59 +0700

    Update PenumpangPrioritas.java

--- code/PenumpangPrioritas.java
+++ code/PenumpangPrioritas.java
@@ -1,1 +1,13 @@
+public class PenumpangPrioritas extends Penumpang {
+    public PenumpangPrioritas(String id, int umur, boolean hamil, int saldo) {
+        super(id, umur, hamil, saldo);
+    }
 
+    public boolean bisaDudukPrioritas() {
+        return umur > 60 || umur < 10 || hamil;
+    }
+
+    public TipePenumpang getTipe() {
+        return TipePenumpang.PRIORITAS;
+    }
+}
