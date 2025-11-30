16b2d07de326a87c50b6c62e0edeee51151125d7
roselinebalqist <alinbalqist@gmail.com>
2025-11-30 15:55:39 +0700

    Update Bus.java

--- code/Bus.java
+++ code/Bus.java
@@ -1,1 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class Bus {
+    private final int KURSI_BIASA = 16;
+    private final int KURSI_PRIORITAS = 4;
+    private final int KAPASITAS_TOTAL = 40;
+    private final int ONGKOS = 5000;
